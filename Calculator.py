5f0a762eff5781255c996c9fbfae5983d4c00c06
Bijay-Gurung <stha4580@gmail.com>
2023-07-01 21:43:41 +0545

    prototype 1

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+def addition():
+    pass
+def subtraction():
+    pass
+def multiplication():
+    pass
+def division():
+    pass
+def modulo():
+    pass
+
+def main():
+    print("Basic Calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Modulo")
+    print("q. Quit")
